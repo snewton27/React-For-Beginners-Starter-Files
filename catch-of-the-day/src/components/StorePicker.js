d221c3273c3c36640e3f8c92ab24c0a23479c9e4
snewton27 <spencer.newton@sky.uk>
2018-11-11 19:04:55 +0000

    L5: More Component Stuff

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Store Picker</p>
+        return (
+            <Fragment>
+                <h1>Store Selector</h1>
+                <form className="store-selector">
+                    <h2>Please Select a Store</h2>
+                    <input type="text" required placeholder="Store Name" />
+                    <button type="submit">Visit Store</button>
+                </form>
+            </Fragment>
+        );
     }
 }
 
