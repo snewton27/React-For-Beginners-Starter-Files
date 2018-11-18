98d7d53c16558ce25eac44d23e114af07d6f95f9
snewton27 <spencer.newton@sky.uk>
2018-11-18 20:34:27 +0000

    L10: stupid names

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
                 <h1>Store Selector</h1>
                 <form className="store-selector">
                     <h2>Please Select a Store</h2>
-                    <input type="text" required placeholder="Store Name" />
+                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                     <button type="submit">Visit Store</button>
                 </form>
             </Fragment>
