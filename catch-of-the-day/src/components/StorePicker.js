b6727f695dd3d2dd4a66b35bd212b85ae9b31f88
snewton27 <spencer.newton@sky.uk>
2018-11-18 20:55:29 +0000

    L12: simple event and refs

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,18 +2,30 @@ import React, { Fragment } from 'react'
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>Store Selector</h1>
-                <form className="store-selector">
-                    <h2>Please Select a Store</h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-                    <button type="submit">Visit Store</button>
-                </form>
-            </Fragment>
-        );
-    }
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log('Going to store!');
+  }
+
+  render() {
+      return (
+          <Fragment>
+              <h1>Store Selector</h1>
+              <form className="store-selector" onSubmit={this.goToStore}>
+                  <h2>Please Select a Store</h2>
+                  <input ref={this.myInput}
+                         type="text"
+                         required
+                         placeholder="Store Name"
+                         defaultValue={getFunName()} />
+                  <button type="submit">Visit Store</button>
+              </form>
+          </Fragment>
+      );
+  }
 }
 
 export default StorePicker
