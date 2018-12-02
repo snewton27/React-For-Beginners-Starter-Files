4bbf67076f4d50595ec18576c99cdf323dedb496
snewton27 <spencer.newton@sky.uk>
2018-12-02 19:58:50 +0000

    L13: Understanding State (adding fishies)

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,33 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Make copy of existing state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Modify copy of state
+    fishes[`fish-${Date.now()}`] = fish;
+
+    // 3. Overwrite original state with new state
+    this.setState({
+      fishes
+    });
+  };
+
     render() {
         return (
           <div className="catch-of-the-day">
             <div className="menu">
                 <Header tagline="Fresh Seafood Market"/>
             </div>
-              <Inventory />
               <Order />
+              <Inventory addFish={this.addFish}/>
           </div>
         );
     };
