2bf972f31c0a884353094d58dc79de082df4074e
snewton27 <spencer.newton@sky.uk>
2018-12-27 17:34:07 +0000

    L16: Adding Fish to and Order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -25,6 +25,19 @@ class App extends React.Component {
     });
   };
 
+  addFishToOrder = key => {
+    // 1. Make copy of existing state
+    const order = { ...this.state.order };
+
+    // 2. Modify copy of state to add new fish or increment counter
+    order[key] = order[key] + 1 || 1;
+
+    // 3. Overwrite original state with new state
+    this.setState({
+      order
+    });
+  };
+
   loadSampleFishes = () => {
     this.setState({
       fishes: sampleFishes
@@ -39,12 +52,12 @@ class App extends React.Component {
               <ul className="fishes">
                 {Object.keys(this.state.fishes).map(key => {
                   return(
-                    <Fish key={key} fishDetails={this.state.fishes[key]} />
+                    <Fish key={key} index={key} addFishToOrder={this.addFishToOrder} fishDetails={this.state.fishes[key]} />
                   )
                 })}
               </ul>
             </div>
-              <Order />
+              <Order order={this.state.order}/>
               <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
           </div>
         );
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,14 +4,20 @@ import { formatPrice } from '../helpers'
 class Fish extends React.Component {
   render() {
     const { fishDetails } = this.props;
+    const isAvailable = fishDetails.status === "available";
     return(
       <li className="menu-fish">
         <img src={fishDetails.image} alt={fishDetails.name} />
-        <h3 className="fish-name">{fishDetails.name}
+        <h3 className="fish-name">
+          {fishDetails.name}
           <span className="price">{formatPrice(fishDetails.price)}</span>
         </h3>
-        <p>{fishDetails.desc} </p>
-        <button>Add To Cart</button>
+        <p>{fishDetails.desc}</p>
+        <button
+          onClick={() => {this.props.addFishToOrder(this.props.index)}}
+          disabled={!isAvailable}>
+            {isAvailable ? 'Add To Cart' : 'Sold Out'}
+        </button>
       </li>
     );
   };
