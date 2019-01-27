25ffbb1dc7e328f58173c27b472b2618dccc49fe
Spencer Newton <sn.newton92@gmail.com>
2019-01-27 18:04:48 +0000

    removing api key

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCqGychV_05_qQT11Aeh8MGX888jo_HBm0",
+  apiKey: "",
   authDomain: "catch-of-the-day-spn1.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-spn1.firebaseio.com"
 });
